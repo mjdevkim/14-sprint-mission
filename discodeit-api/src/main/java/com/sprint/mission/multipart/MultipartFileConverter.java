7508f5f9e1d5aabe2915882006ca0cbcde472fb0
Minjae Kim <137mjkim@gmail.com>
2026-09-20 12:12:31 +0900

    refactor: 싱글모듈로 구조 변경

--- discodeit-api/src/main/java/com/sprint/mission/multipart/MultipartFileConverter.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.mission.multipart;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface MultipartFileConverter {
-    MultipartFileDto convert(MultipartFile file);
-}
